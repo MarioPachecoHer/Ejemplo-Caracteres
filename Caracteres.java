b6fb789e7c151156f0d28145c7734e2dafe01fe6
MarioPachecoHer <20243rd005@utez.edu.mx>
2025-05-06 09:15:59 -0600

    Caracteres en java

---
+++ Caracteres.java
@@ -0,0 +1,11 @@
+public class Caracteres {
+    public static void main(String[] args) {
+        char letra = 'J';
+        char simbolo = '@';
+        char numero = '5';
+
+        System.out.println("letra =" + letra);
+        System.out.println("simbolo =" + simbolo);
+        System.out.println("numeroChar =" + numero);
+    }
+}
