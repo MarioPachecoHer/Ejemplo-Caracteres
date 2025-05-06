a5174d5b15ba13ff3b1184a76c195320854cc9b2
MarioPachecoHer <20243rd005@utez.edu.mx>
2025-05-06 09:17:12 -0600

    Hice cambios

--- Caracteres.java
+++ Caracteres.java
@@ -4,8 +4,6 @@ public class Caracteres {
         char simbolo = '@';
         char numero = '5';
 
-        System.out.println("letra =" + letra);
-        System.out.println("simbolo =" + simbolo);
-        System.out.println("numeroChar =" + numero);
+
     }
 }
